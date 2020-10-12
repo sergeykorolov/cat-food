010c10537a39bfb790b4b8e16b72065b355c0dd7
sergeyorolov <korolovsergio@mail.ru>
2020-10-12 22:21:19 +0300

    Updates

---
+++ precache-manifest.af883211c1e1dea64dfc508941793583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b6c171f8ad5c53100af457101a1acf",
+    "url": "/cat-food/index.html"
+  },
+  {
+    "revision": "d97aee21adeffa9c48a8",
+    "url": "/cat-food/static/css/main.ed935409.chunk.css"
+  },
+  {
+    "revision": "21934cb68f30a11e2925",
+    "url": "/cat-food/static/js/2.e0b21ce0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cat-food/static/js/2.e0b21ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97aee21adeffa9c48a8",
+    "url": "/cat-food/static/js/main.a10ad956.chunk.js"
+  },
+  {
+    "revision": "aa1756814718f8fcab70",
+    "url": "/cat-food/static/js/runtime-main.2a8d3a4e.js"
+  },
+  {
+    "revision": "78ad2bee5a41d73b1bd69a21861eda47",
+    "url": "/cat-food/static/media/cat.78ad2bee.png"
+  }
+]);
